1a857115624e478234a4ab94853fe4073962b961
davidpiper89 <113616933+davidpiper89@users.noreply.github.com>
2022-10-10 15:15:25 +0100

    Add files via upload

---
+++ JS Lesson 1/task.js
@@ -0,0 +1,37 @@
+// 1
+
+var firstName = "David"
+var age = 32
+var male = true
+
+var firstName1 = "Helen"
+var age1 = 44
+var male = false
+
+// 2
+
+console.log(age+age1)
+
+//3 
+
+console.log(firstName+firstName1)
+
+// 4/5/6
+
+if (firstName===firstName1 ) {
+    console.log("Name matches")
+} else if (!firstName===!firstName1 || age<age1) {
+    console.log("No Match but one is older than the other")
+}
+
+
+// 7
+
+personalInf ["David Piper", age=32, height=6.3]
+
+// 8
+
+daysOfWeek ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+
+
